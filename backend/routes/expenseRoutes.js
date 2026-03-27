0e065370bee191bcbd67172a19a5afdd328f8f0e
Vinay Kumar <vin.kp.1305@gmail.com>
2026-03-27 18:56:31 +0530

    changes in expenseRoutes file

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -9,11 +9,12 @@ const {
   getSummary,
   correctCategory
 } = require("../controllers/expenseController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/expenses", createExpense);
-router.get("/expenses", getExpenses);
-router.get("/expenses/summary", getSummary);
-router.get("/expenses/:id", getExpenseById);
-router.delete("/expenses/:id", deleteExpense);
-router.put("/expenses/:id/correct", correctCategory);
+router.post("/expenses",authMiddleware, createExpense);
+router.get("/expenses", authMiddleware,getExpenses);
+router.get("/expenses/summary",authMiddleware, getSummary);
+router.get("/expenses/:id", authMiddleware,getExpenseById);
+router.delete("/expenses/:id", authMiddleware,deleteExpense);
+router.put("/expenses/:id/correct", authMiddleware,correctCategory);
 module.exports = router;
