33b8074d42446ec874aa886a72daaa06aff75391
Vinay Kumar <vin.kp.1305@gmail.com>
2026-03-28 14:17:35 +0530

    Added frontend UI improvements, modal, edit/delete functionality and backend update API and bug fixes

---
+++ frontend/src/app/components/ProtectedRoutes.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function ProtectedRoute({ children }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  if (loading) return <p>Checking auth...</p>;
+
+  return children;
+}
